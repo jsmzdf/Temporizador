fcaf47aeaf43401228f4866f21a2b3f92eb8ea4f
jsmzdf <sebastianjhon9427@gmail.com>
2020-05-04 12:12:33 -0500

    se crear coronometro

--- Logica.py
+++ Logica.py
@@ -8,39 +8,43 @@ Created on Mon May  4 10:45:07 2020
 
 
 import time
-class Logica:
+class Temporizador:
     def __init__(self):
         self.tiempoT=0
 
-    def igresoTTotal(self,tiempoT):
+    def ingresoTTotal(self,tiempoT):
         self.tiempoT=tiempoT
         
-    def diviMin(self,muni):
-        pass
-    def cornometro(self):
-        pass 
+    def mostrar(self,i,segu,mesaje):
         
+        return i,segu,mesaje
+    
         
+    def contar(tiempo,ejercicio):
+        
+        for i in range(1,tiempo+1):
+          tiempooras=i
+          segun=0
+          minu=0
+          x='q'
+          mesaje="ejecicio"
+          while (x !='ok'):
+            segun=segun+1 
+            minu=minu+1          
+            if(segun==60):
+                segun=0
+                
+            print(minu)
+            print(str(i)+':'+str(segun))
+            
+            if(segun==ejercicio):
+               mesaje="descanso"      
+                
+            if(minu==60):
+                x='ok'
+            time.sleep(1)
+    
+horas=input('ingrese tiempo total')
 
-def contar(h,m,s):
-       horas=0
-       segun=0
-       minu=0
-       #horas<=h and segun<=s or minu<=m 
-       x='q'
-       while (x !='ok'):
-           segun=segun+1           
-           if(segun==60):
-               segun=0
-               minu=minu+1
-           if(minu==60):    
-               minu=0
-               horas=horas+1
-           print(str(horas)+':'+str(minu)+':'+str(segun))
-           if(horas==h and minu==m and segun==s):
-               x='ok'
-           time.sleep(1)
-horas=input('ingrese hora')
-minutos=input('ingrese minutos')
-segundos=input('ingrese segundos')
-limite=contar(int(horas),int(minutos),int(segundos))
+ejercicio=input('ingrese segundos')
+Temporizador.contar(int(horas),int(ejercicio))
